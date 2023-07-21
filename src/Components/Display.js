94c9663b97cf0bb3b6d9c95e7cafd3d112bb6be9
Ramesh3282 <ramesharthanari.a@gmail.com>
2023-07-21 16:34:08 +0530

    Removed unwanted components

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -5,7 +5,7 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ModeIcon from '@mui/icons-material/Mode';
 import { useNavigate } from 'react-router-dom';
-import Addstudent from './Addstudent';
+
 
 
 
